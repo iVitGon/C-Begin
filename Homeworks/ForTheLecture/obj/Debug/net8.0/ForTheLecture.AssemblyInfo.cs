ffa245553cde384537a6d89c81979f2dea9e9959
Vitus <4665711@gmail.com>
2024-03-07 11:00:49 +0300

    Добавлен .ignore

--- Homeworks/ForTheLecture/obj/Debug/net8.0/ForTheLecture.AssemblyInfo.cs
+++ Homeworks/ForTheLecture/obj/Debug/net8.0/ForTheLecture.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ForTheLecture")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afd8e65310547a43985014f4114119575b84783b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ForTheLecture")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ForTheLecture")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
