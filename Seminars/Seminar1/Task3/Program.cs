7c3e3adbbb1c2f37e677fb4f7233ca9f7cc1284f
Vitus <4665711@gmail.com>
2024-03-07 21:03:06 +0300

    Число от -n до n

---
+++ Seminars/Seminar1/Task3/Program.cs
@@ -0,0 +1,15 @@
+﻿//Напишите программу, которая на вход принимает
+//целое число N, а на выходе показывает все целые
+//числа в промежутке от -N до N.
+//Примеры
+//4 => -4, -3, -2, -1, 0, 1, 2, 3, 4
+//2 => -2, -1, 0, 1, 2
+
+System.Console.WriteLine("Введите число n");
+int n = Convert.ToInt32(Console.ReadLine());
+int count = -n;
+while (count <= n)
+{
+    System.Console.Write(count+" ");
+    count ++;
+}
