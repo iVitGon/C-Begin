79c4c887d1ffb3e45dea933c2e7622da7122e5e8
Vitus <4665711@gmail.com>
2024-03-07 20:52:09 +0300

    Разные типы данных

---
+++ Seminars/Seminar1/Task2/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Как тебя зовут");
+string name = Console.ReadLine();
+System.Console.WriteLine($"Привет, {name}!");
+System.Console.WriteLine("Сколько тебе лет?");
+int age = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine($"Класс! Мне тоже {age}");
+
