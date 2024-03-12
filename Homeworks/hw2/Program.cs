84cc1fc2d0e9ee4faf4ac4719b7d3c2755cf4804
Vitus <4665711@gmail.com>
2024-03-12 10:41:31 +0300

    Мето void  и соответствие двойному условию

---
+++ Homeworks/hw2/Program.cs
@@ -0,0 +1,20 @@
+﻿void NumMultiplicity23And7 (int a)
+{
+    if (a % 7 == 0 && a % 23 == 0)
+    {
+        System.Console.WriteLine($"Число {a} кратно 7 и 23");
+    }
+    else
+    {
+        System.Console.WriteLine($"Чмсло {a} НЕкратно 7 и 23");
+    }
+}
+
+
+System.Console.WriteLine("Кратность числа 7 и 23");
+System.Console.WriteLine();
+System.Console.WriteLine("Введите число для проверки");
+int number = Convert.ToInt32(Console.ReadLine());
+
+NumMultiplicity23And7(number); // void метод
+
