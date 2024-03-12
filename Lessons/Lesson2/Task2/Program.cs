639022a4e0a0b026366dbc75d66b97747e18b887
Vitus <4665711@gmail.com>
2024-03-12 21:26:18 +0300

    Задача2.Заполнение массива

---
+++ Lessons/Lesson2/Task2/Program.cs
@@ -0,0 +1,11 @@
+﻿int n = 10;
+int[] arr = new int[n];
+int i = 0;
+
+while(i < arr.length) // для заполнения используем цикл
+{
+    arr[i] = i + 1;
+    System.Console.Write(arr[i]);
+    System.Console.Write(" ");
+    i = i + 1;
+}
