e47c56413ae287ca39cbb8812037ae67ae38f0e4
Vitus <4665711@gmail.com>
2024-03-12 20:36:30 +0300

    Задача1.Создание первого массива

---
+++ Lessons/Lesson2/Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿
+ int[] arr  = new int [5];// создаем говый массив с выделением памяти для 5 элементов
+                         // если нужен был массив из вещественных чисел, тип данных double или float
+arr [0] = 5;// Заполняем массив, обращаясь к кажой ячейки по индексу
+arr [1] = 7;
+arr [2] = 5;
+arr [3] = 4;
+arr [4] = 3;
+
+System.Console.WriteLine(arr[0]);
+System.Console.WriteLine(arr[1]);
+System.Console.WriteLine(arr[2]);
+System.Console.WriteLine(arr[3]);
+System.Console.WriteLine(arr[4]);
