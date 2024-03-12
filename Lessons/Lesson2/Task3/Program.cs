aa12cb682103ceccaa65c1ff6809451220c27c6d
Vitus <4665711@gmail.com>
2024-03-12 21:26:50 +0300

    Задача3.Заполнение массива Проверка на четность

---
+++ Lessons/Lesson2/Task3/Program.cs
@@ -0,0 +1,13 @@
+﻿int n = 10;
+int[] arr = {1,4,6,8,45,34,44,56,2,15};
+int i = 0;
+
+while(i < arr.Length)
+{
+    if (arr[i] % 2 == 0)
+    {
+        Console.Write ($"{arr[i]} ");
+        
+    }
+    i = i + 1;
+}
