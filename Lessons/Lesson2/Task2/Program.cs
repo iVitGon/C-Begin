ad95186ceadc8965d972ab4e29156ace222ef53d
Vitus <4665711@gmail.com>
2024-03-14 11:06:34 +0300

    Задача2.Создание массива+наполнение его значениями кратными 11+ вывод в консоль четных значений

--- Lessons/Lesson2/Task2/Program.cs
+++ Lessons/Lesson2/Task2/Program.cs
@@ -1,11 +1,22 @@
 ﻿int n = 10;
 int[] arr = new int[n];
 int i = 0;
-
-while(i < n) // для заполнения используем цикл
+int j = 11;
+while(i < n ) // для заполнения используем цикл
 {
-    arr[i] = i + 1;
-    System.Console.Write(arr[i]);
+    arr[i] = j;//В теле цикла cформируем очередной элемент массива i+1, поместим его на позицию i
+    System.Console.Write(arr[i]); // выведем на экран
     System.Console.Write(" ");
-    i = i + 1;
+    j = j + 11;
+    i = i + 1; // Перейдём к следующему значению переменной-счётчика, увеличив её на 1.
+}
+ System.Console.WriteLine();
+int k = 1;
+while(k < arr.Length)// мохно испольховать i < nc
+{
+    if (arr[k] % 2 == 0)
+    {
+        Console.Write ($"{arr[k]} ");        
+    }
+    k = k + 1;
 }
