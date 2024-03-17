700c95d7b2c3bcd57da6db4ff249cb69087f64fe
Vitus <4665711@gmail.com>
2024-03-17 10:17:48 +0300

    Задача3. Void. Обнуление четных чисел и печать получившегося массива

---
+++ Lessons/Lesson3/Task3/Program.cs
@@ -0,0 +1,25 @@
+﻿void ZeroEvenElemets(int[] array)
+{
+
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            array[i] = 0;
+        }
+
+    }
+}
+
+void PrintArray(int [] array)
+{
+    foreach (int e in array)
+    {
+        Console.Write($"{e} ");
+    }
+}
+
+int [] arr = {1,2,3,4,5};
+ZeroEvenElemets(arr); // при передаче параметров тип данных не указываем
+PrintArray(arr);
