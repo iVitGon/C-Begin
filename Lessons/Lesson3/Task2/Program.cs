0a0682bac83e2a0c24a105b60e63d5087cb17fc6
Vitus <4665711@gmail.com>
2024-03-17 09:11:55 +0300

    Задача2. Void метод

---
+++ Lessons/Lesson3/Task2/Program.cs
@@ -0,0 +1,13 @@
+﻿void PrintSquare (int limit)
+{
+    int i = 1;
+    while (i <= limit)
+    {
+        System.Console.Write($"{i*i} ");
+        i = i + 1; // i++
+    }
+}
+
+PrintSquare(5);
+PrintSquare(10);
+PrintSquare(15);
