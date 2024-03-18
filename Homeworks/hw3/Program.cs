26700890355e4d4fea03ef2759038a84041a5459
Vitus <4665711@gmail.com>
2024-03-18 15:02:42 +0300

    Задача 2. Создание функций. Сортировка массива чет/нечет

--- Homeworks/hw3/Program.cs
+++ Homeworks/hw3/Program.cs
@@ -1,6 +1,8 @@
 ﻿// Задайте одномерный массив из 10 целых чисел от 1 до 100. Найдите количество элементов 
 // массива, значения которых лежат в отрезке [20,90]. 
 
+/*
+
 void FillArray(int [] array)
 {
     Random rnd = new Random(); // выделение памяти для переменной rnd  типа данных Random
@@ -24,9 +26,9 @@ void PrintArray(int [] array)
 
 int SortArray20to90(int [] array)
 {
-    int sum = 0;
-    int nsum = 0;
-    int i = 0;
+    int sum = 0; // счетчик, отражающий количество подходящих под параметры элементов
+    int nsum = 0;// счетчик остальных элементов
+    int i = 0;// счетчик для элементов массива
     while (i < array.Length)
     {
         if (array[i] >= 20 && array[i] <= 90)
@@ -54,6 +56,58 @@ int result = SortArray20to90(arr);
 
 System.Console.WriteLine(result);
 
+*/
 
+
+
+// Задача 2: Задайте массив на 10 целых чисел. Напишите программу, которая определяет 
+// количество чётных чисел в массиве.
+
+void FillRandomArray (int [] array)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(1, 1000); 
+    }
+
+}
+
+void PrintArray(int [] array)
+{
+    foreach (int e in array)
+    {
+        System.Console.Write($"{e} ");
+    }
+}
+
+int SortOfEvenArray(int [] array)
+{
+    int even = 0;
+    int notEven = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            even = even + 1;
+
+        }
+        else
+        {
+            notEven = notEven + 1;
+        }
+    }
+    return even;
+}
+
+int size = 10;
+int [] arr = new int[size];
+
+FillRandomArray(arr);
+PrintArray(arr);
+System.Console.WriteLine();
+int result = SortOfEvenArray(arr);
+
+System.Console.WriteLine(result);
 
 
