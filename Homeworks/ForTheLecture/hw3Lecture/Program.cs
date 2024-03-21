5be49bdcc688bc8426bed54c2baec7b61ab09cb5
Vitus <4665711@gmail.com>
2024-03-21 19:16:36 +0300

    Создание массива из чисельно-буквенной строки, введенной с консоли

---
+++ Homeworks/ForTheLecture/hw3Lecture/Program.cs
@@ -0,0 +1,93 @@
+﻿/*
+считать с консоли символы, содержащие цифры и латинские буквы.
+сформировать массив, состоящий из цифр из введенной строки
+Например, 123dfg34 -> [1 2 3 3 4]
+Разделить логику алгоритма на функции
+Память под массив выделять необходимого размера (не больше, чем количество элементов массива)
+
+*/
+int GetNumbersFromString (string symbol)
+{
+    int countSize = 0;
+    string digit = "";
+    foreach (char symb in symbol)
+    {
+        if (char.IsAsciiDigit(symb) == true)
+        {
+            digit = digit + 1;
+            countSize = countSize + 1;
+        }
+  
+    }
+    return countSize;
+
+} 
+
+string GetDigitFromString (string symbol)
+{
+    string letter = "";
+    string digit = "";
+    foreach (char symb in symbol)
+    {
+        if (char.IsAsciiLetter(symb) == true)
+        {
+            letter = letter + symb;
+            
+        }
+        else
+        {
+            digit = digit + symb;
+        }
+  
+    }
+    return digit;
+
+} 
+
+int [] FillArray (int[] array, int digits) 
+{
+    for (int j = 0; j < array.Length; j++)
+    {
+        if (digits > 0)
+        {
+            array[j] = digits % 10;
+            digits = digits /10;
+        }
+    }
+    return array;
+}
+
+void PrintArray(int [] array)
+{
+    foreach (int item in array)
+    {
+        System.Console.Write($"{item} ");
+    }
+}
+
+int [] ReverseArray(int [] arr)
+{
+    int temp = 0;
+    for (int i = 0; i < arr.Length / 2; i++)
+    {
+        temp = arr[i];
+        arr[i] = arr[arr.Length - 1 - i];
+        arr[arr.Length - 1 - i] = temp;
+    }
+    return arr;
+}
+
+System.Console.WriteLine("Введите символы ");
+string symbol = Console.ReadLine();
+
+int size = GetNumbersFromString(symbol);
+System.Console.WriteLine(size);
+string digit = GetDigitFromString(symbol);
+System.Console.WriteLine(digit);
+int[] array = new int [size];
+int digits = Convert.ToInt32(digit);
+int [] arr = FillArray(array,digits);
+PrintArray(arr);
+System.Console.WriteLine();
+int [] reverseArr = ReverseArray(arr);
+PrintArray(reverseArr);
--- Lessons/Lesson4/Program.cs
+++ Lessons/Lesson4/Program.cs
@@ -1,4 +1,6 @@
-﻿int [,] matrix = new int [3,5]; // выделить память на три строки и пять элементов
+﻿
+/*
+int [,] matrix = new int [3,5]; // выделить память на три строки и пять элементов
 
 Random rnd = new Random();
 
@@ -54,3 +56,115 @@ void PrintMatrix(int [,] matrix)
 
 int [,] matrixNew = CreareMatrix (4, 5);
 PrintMatrix(matrixNew);
+
+*/
+
+
+// Интересный массив
+/*
+
+int[,] CreareMatrix(int rowCount, int columnCount)
+{
+    int[,] matrix = new int[rowCount, columnCount];
+
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++) // GetLength (0) запрашиваем количество строк в двухмерном массиве
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            //Random rnd = new Random(); - операция "ресурсоемкая", поэтому внутри цикла лучше не использовать
+            matrix[i, j] = rnd.Next(1, 1000);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int [,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++) 
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        Console.Write($"{matrix[i, j]} ");
+    }
+    Console.WriteLine();
+}
+}
+
+int[,] matrix = CreareMatrix(3, 4);
+PrintMatrix(matrix);
+
+foreach (int e in matrix)
+{
+    if (IsInteresting(e) == true)
+    {
+        Console.WriteLine($"{e} ");
+    }
+}
+
+bool IsInteresting(int value)
+{
+    int sumOfDigit = GetSumOfDigits(value);
+    if (sumOfDigit % 2 == 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+int GetSumOfDigits(int value)
+{
+    int sum = 0;
+    while(value > 0)
+    {
+        sum = sum + value % 10;
+        value = value / 10;
+    }
+    return sum;
+}
+
+*/
+
+
+
+// Создать строку, состоящую из букв, приняв на вход чисельно - буквенные символы
+
+/* Этапы решения
+1. string - это массив char. Т.е. каждая буква в строке лежит под в ячейке род своим индексом
+string str = "Hello, World";
+System.Console.WriteLine(str[11]); ---> d
+2. Создане строк (3 варианта)
+ - string s1 = "Hello";
+ - char [] ch_array ={'H','e','l','l','o'};
+ - string s2 = new String (ch_array);
+
+3. принять строку
+4. проход по каждому элементу
+5. Буква или нет
+6. Если буква, то дописать (конкатенация, т.е. к концу одной строки преписывает начало другой)
+Конкатиеация - оператор "+"
+
+*/
+string GetLettersFromString(string s)
+{
+    string letters = "";
+    foreach (char e in s)
+    {
+        if (char.IsAskiiLetter(e) == true)
+        {
+            letters = letters + e;
+        }
+    }
+    return letters;
+}
+
+
+string str = Console.ReadLine();
+string result = GetLettersFromString(str);
+System.Console.WriteLine(result);
+
+
